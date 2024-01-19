e866571d36389123bbdeaefb88edb3efbd4c7098
Loup RUSAK <74204559+louprusak@users.noreply.github.com>
2024-01-19 10:47:46 +0100

    Initial Commit

---
+++ TripWay IOS/TripWay_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripWay_IOSApp.swift
+//  TripWay IOS
+//
+//  Created by Loup RUSAK on 19/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct TripWay_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
