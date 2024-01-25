7e93dd7b3b33a8183ee0c3588d132c13be446882
Loup RUSAK <74204559+louprusak@users.noreply.github.com>
2024-01-25 09:24:13 +0100

    Premier ajout des fichiers

---
+++ TripWay IOS/ViewComponents/HomeTripTile.swift
@@ -0,0 +1,8 @@
+//
+//  HomeTripTile.swift
+//  TripWay IOS
+//
+//  Created by Loup RUSAK on 19/01/2024.
+//
+
+import Foundation
---
+++ TripWay IOS/Views/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  TripWay IOS
+//
+//  Created by Loup RUSAK on 24/01/2024.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
